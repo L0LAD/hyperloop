d650ae3ba757ebd29c0b1ca484ae9a645e1cedf2
L0LAD <audrey.denoual@live.fr>
2021-04-22 19:54:03 +0200

    Installation composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0783afce692cfef303bec3a4f70b2305b32c4a52',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0783afce692cfef303bec3a4f70b2305b32c4a52',
+    ),
+    'eftec/bladeone' => 
+    array (
+      'pretty_version' => '3.52',
+      'version' => '3.52.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a19bf66917de0b29836983db87a455a4f6e32148',
+    ),
+  ),
+);
